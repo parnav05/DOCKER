7958f71de64db1da92c016306196b1069ff5406e
parnav <parnavdev05@gmail.com>
2026-05-09 11:56:25 +0000

    edited config.py

--- docker_compose/project1-python-url-shortener/app/config.py
+++ docker_compose/project1-python-url-shortener/app/config.py
@@ -1,11 +1,18 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
     database_url: str
     redis_url: str
-    base_url: str = "http://localhost:8000"
+    base_url: str
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
